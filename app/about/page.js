76c9027302cfecb1ab9106101f746bb0459d8ad9
prajwal <prajju723nayak@gmail.com>
2025-11-22 08:27:38 +0530

    dark mode to about us

--- app/about/page.js
+++ app/about/page.js
@@ -10,44 +10,44 @@ export default function about(){
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Marcellus&display=swap');
         </style>
-        <div className="lg:hidden bg-neutral-200">
+        <div className="lg:hidden bg-neutral-200 dark:bg-neutral-900">
             <Navbar/>
             <Herosection title="About Us"/>
-            <div className="w-screen flex  bg-neutral-200 flex justify-center ">
+            <div className="w-screen flex  bg-neutral-200  justify-center dark:bg-neutral-900">
                
-                <span className="font-[Marcellus] text-green-700 text-lg font-bold  ">OUR TRUSTED 8K HAPPY PARTNER</span>
+                <span className="font-[Marcellus] text-[#826a45] text-lg font-bold  ">OUR TRUSTED 8K HAPPY PARTNER</span>
               
             </div>
-            <div className="flex flex-row bg-neutral-200 justify-center gap-4 h-40 pt-5"> 
+            <div className="flex flex-row bg-neutral-200 justify-center gap-4 h-40 pt-5 dark:bg-neutral-900"> 
                       <img src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fbrand%2F3.png&w=1080&q=75" className="h-30 w-35"/>
                     <img src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fbrand%2F4.png&w=1080&q=75" className="h-30 w-35"/>
             </div>
             <div className="h-1/3 w-screen bg-black">
                     <img src="https://restan-nextjs.vercel.app/assets/img/about/2.jpg" className="h-full"/>
             </div>
-            <div>
-                <div className="font-[Marcellus] text-green-700 text-lg font-bold my-3 bg-neutral-200">
+            <div className="mx-3">
+                <div className="font-[Marcellus] text-[#826a45] text-lg font-bold my-3 bg-neutral-200 dark:bg-neutral-900">
                         About us
                 </div>
-                <div className="font-[Marcellus] text-black text-3xl font-normal mb-3 bg-neutral-200">
+                <div className="font-[Marcellus] text-black text-3xl font-normal mb-3 bg-neutral-200 dark:bg-neutral-900 dark:text-white">
                         We Invite You
                         To Visit Our Restaurant
                 </div>
-                <div className="text-neutral-500 text-sm mb-3 bg-neutral-200">
+                <div className="text-neutral-500 text-sm mb-3 bg-neutral-200 dark:bg-neutral-900 dark:text-neutral-400">
                     A relaxing and pleasant atmosphere, good jazz, dinner, and cocktails. The Patio Time Bar opens in the center of Florence. The only bar inspired by the 1960s, it will give you a experience that you'll have a hard time forgetting.A relaxing and pleasant atmosphere, good jazz, dinner, and cocktails. The Patio Time Bar opens in the center of Florence. The only bar inspired by the 1960s, it will give you a experience that you'll have a hard time forgetting.
                 </div>
-                <div className="bg-stone-500 text-white font-normal text-sm px-5  w-40 ml-5 h-7 flex justify-center pb-8 pt-3 bg-yellow-700">
+                <div className=" text-white font-normal text-sm px-5  w-40 ml-5 h-7 flex justify-center pb-8 pt-3 bg-yellow-700 ">
                         DISCOVER MORE
                 </div>
             </div>
             <div>
-                <div className="font-[Marcellus] text-lg text-green-700 font-bold flex justify-center pt-20 pb-5">
+                <div className="font-[Marcellus] text-lg text-[#826a45] font-bold flex justify-center pt-20 pb-5">
                 MASTER CHEFS 
             </div>
-            <div className="font-[Marcellus] text-3xl ml-6 text-black font-bold">
+            <div className="font-[Marcellus] text-3xl  text-black font-bold text-center dark:text-white">
                 Meet Our Special Chefs
             </div>
-                          <div className="ml-[-5px]">
+                          <div className="ml-[-2px]">
                                  <ChefComp imagee={"https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fteam%2F1.jpg&w=828&q=75"} namee={"Alexander Petllo"} role={"ASSISTANT CHEF"} />
                                   <ChefComp imagee={"https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fteam%2F2.jpg&w=828&q=75"} namee={"Mendia Juxef"} role={"BURGER KING"} />
                                               <ChefComp imagee={"https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fteam%2F3.jpg&w=828&q=75"} namee={"Petro William"} role={"MAIN CHEF"} />
@@ -56,14 +56,14 @@ export default function about(){
             </div>
 
                     <div className="flex flex-row justify-center pt-10">
-                                       <iframe src="https://youtube.com/embed/F3zw1Gvn4Mk"/ >
+                                       <iframe src="https://youtube.com/embed/F3zw1Gvn4Mk?autoplay=1&mute=1&controls=0&loop=1&playlist=F3zw1Gvn4Mk"/>
 
                     </div>
                <div>
                            <div className="font-[Marcellus] text-3xl ml-6 text-black font-bold mt-8 mb-5">
                                 Opening Hours
                            </div>
-                           <div className="text-neutral-500 text-sm mb-3 bg-neutral-200 mb-5">
+                           <div className="text-neutral-500 text-sm mb-3 bg-neutral-200 mb-5 dark:bg-neutral-900">
                             A relaxing and pleasant atmosphere, good jazz, dinner, and cocktails. The Patio Time Bar opens in the center..
                            </div>
                            <div className="text-black font-semibold mb-5">
